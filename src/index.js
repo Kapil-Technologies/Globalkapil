489a6caf3f9a1b0b72df7f5b96f301de5d7776d1
Soumya Balamaala <soumyab.companies@gmail.com>
2023-10-03 18:14:52 +0530

    Changes in Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import ReactGA from "react-ga4";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 ReactGA.initialize("G-62KEVNGNVQ");
 root.render(
   <Router>
@@ -13,14 +13,14 @@ root.render(
   </Router>
 );
 
+ReactGA.pageview(window.location.pathname + window.location.search);
+ReactGA.event({ category: "Button", action: "Click", label: "Contact Us" });
+
 const SendAnalytics = () => {
   ReactGA.send({
     hitType: "Kapil Technologies India",
     page: window.location.pathname,
-  
-  
   });
 };
 
 reportWebVitals(SendAnalytics);
-
